e372fcea950d1df0a71d84d3a3fb5ec8dbb6024f
Vadim Ivanov <vadim1ivanov@yandex.ru>
2021-08-25 00:01:34 +0300

    Change config

--- src/js/utils/config.js
+++ src/js/utils/config.js
@@ -1,11 +1,13 @@
-// const serverUrl = NODE_ENV === 'development' ? 'http://api.newsexp.ml' : 'https://api.newsexp.ml'
-const serverUrl = NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://localhost:3000'
+const serverUrl = NODE_ENV === 'development' ? 'http://api.newsexp.ml' : 'https://api.newsexp.ml'
+// const serverUrl = NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://localhost:3000'
 const obj = {
   baseUrl: `${serverUrl}`,
-  newsApiUrl: 'http://newsapi.org/v2/everything?',
-  newsApiUrlDate: 'from=2020-07-25&',
+  // newsApiUrl: 'https://newsapi.org/v2/everything?',
+  newsApiUrl: 'https://nomoreparties.co/news/v2/everything?',
+  // newsApiUrlDate: 'from=2020-07-25&',
+  newsApiUrlDate: '',
   newsApiUrlSortBy: 'sortBy=popularity&',
   newsApiUrlKey: 'apiKey=3612e32c0c1947be80826cf89f04f916',
-  };
+};
 
 export const config = JSON.stringify(obj);
